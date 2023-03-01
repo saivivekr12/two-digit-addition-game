84a9500857521666977b1dafc1a2a963c27eba74
saivivek raavi <saivivekraavi@saiviveks-MacBook-Air.local>
2023-03-01 11:08:01 +0530

    added-comment

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,4 +1,4 @@
-const colors = ["#db5f12", "#0ba6ff","#898989","#24a0ed","#ff00ff","#fd0e35","#1d344a","#08cad1","#9d94ff","#c780e8"];
+const colors = ["#db5f12", "#0ba6ff", "#898989", "#24a0ed", "#ff00ff", "#fd0e35", "#1d344a", "#08cad1", "#9d94ff", "#c780e8"];
 
 let operand1;
 let operand2;
@@ -9,7 +9,7 @@ function generateOperands() {
   operand2 = Math.floor(Math.random() * (100 - operand1)) + 1;
   100 - operand1;
 }
-
+//not to repeat the answer
 function checkOperandsEqualsWithAnswer() {
   const isrepeatedAnswers = questions.some((question) => {
     return parseInt(question.answer) === operand1 + operand2;
@@ -29,7 +29,7 @@ export function generateQuestions() {
       id: i,
       question: `${operand1} + ${operand2}`,
       answer: `${operand1 + operand2}`,
-      color:`${colors[i-1]}`,
+      color: `${colors[i - 1]}`,
     });
   }
   return questions;
